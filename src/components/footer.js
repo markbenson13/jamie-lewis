df5a68b018428140daecd8e58f96a75dbfd3e3f5
Mark Benson Opeña <markbensonopena@gmail.com>
2019-11-07 23:31:22 +0800

    Initial gatsby setup

---
+++ src/components/footer.js
@@ -0,0 +1,42 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import { Nav } from "react-bootstrap"
+
+const Footer = ({ siteTitle }) => (
+    <footer className="footer-wrapper">
+        <div className="container">
+            <div className="row align-items-center">
+                <div className="col-6">
+                    <Nav>
+                        <Nav.Item>
+                            <Nav.Link href="/home">Work</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/about">About</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/music">Music</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/contact" eventKey="#contact">Contact</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </div>
+                <div className="col-6">
+                    <p className="copyright">© {new Date().getFullYear()} Copyright. All rights reserved. Designed by Mark Benson Opena.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+)
+
+Footer.propTypes = {
+    siteTitle: PropTypes.string,
+}
+
+Footer.defaultProps = {
+    siteTitle: ``,
+}
+
+export default Footer
---
+++ src/components/header.js
@@ -0,0 +1,45 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import { Nav } from "react-bootstrap"
+
+const Header = ({ siteTitle }) => (
+  <header className="header-wrapper">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-8">
+          <Nav
+            activeKey="/home"
+          >
+            <Nav.Item>
+              <Link className="nav-link" to="/">Work</Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link className="nav-link" to="/about/">About</Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link className="nav-link" to="/music/">Music</Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="#contact">Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+        <div className="col-4 social-media-wrapper">
+          <a href="#" className="smi linkedin">Linkedin</a>
+          <a href="#" className="smi twitter">Twitter</a>
+        </div>
+      </div>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
