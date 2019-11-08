f4f42289945e4a4b1f081582342fc000d8bc7252
Mark Benson Opeña <markbensonopena@gmail.com>
2019-11-08 09:22:59 +0800

    Add featured video and video items

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
 
-      <main>{children}</main>
+      <main className="main-wrapper">{children}</main>
 
       <Footer siteTitle={data.site.siteMetadata.title} />
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,36 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div className="container">
+      <div className="row">
+        <div className="col-5 featured-video">
+          <h1 className="site-title">Jamie Lewis</h1>
+          <hr />
+          <h2>Music Composition & Sound Design</h2>
+          <h3 className="video-title">In-Game animations by Secret Sauce Studios</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <a href="http://extinction.com/" target="_blank" title="external link">extinction.com/</a>
+        </div>
+        <div className="col-7">
+          <iframe title="vimeo-player" src="https://player.vimeo.com/video/266492994" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+
+      <div className="row video-items">
+        <div className="col-4">
+          <iframe src="https://player.vimeo.com/video/188828855" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <p><a href="https://vimeo.com/188828855">The Guardians - Elite Dangerous: Horizons Trailer</a> from <a href="https://vimeo.com/audioshowreel">Jamie Lewis</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+        </div>
+        <div className="col-4">
+          <iframe src="https://player.vimeo.com/video/200855187" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <p><a href="https://vimeo.com/200855187">Planet Coaster- Official Launch Trailer</a> from <a href="https://vimeo.com/audioshowreel">Jamie Lewis</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+        </div>
+        <div className="col-4">
+          <iframe src="https://player.vimeo.com/video/232099163" width="640" height="273" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <p><a href="https://vimeo.com/232099163">1000 Animated Trailer</a> from <a href="https://vimeo.com/audioshowreel">Jamie Lewis</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+        </div>
+      </div>
+    </div>
     <h1>Hello World!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
