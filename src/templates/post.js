44bab6f3b3dcebb2fed6c76dd36518b9fa30d130
Benson Opeña <bopena@cambridge.org>
2020-02-09 18:39:33 +0800

    Fix video content not showing in production

--- src/templates/post.js
+++ src/templates/post.js
@@ -12,32 +12,27 @@ export default function Template({ data }) {
     const { markdownRemark: post } = data;
 
     return (
-        <Layout>
-            <SEO title={post.frontmatter.videoTitle} />
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12 col-md-5 featured-video">
-                        <h1 className="site-title">{post.frontmatter.videoTitle}</h1>
-                        <hr />
-                        {/* <h2>Music Composition & Sound Design</h2> */}
-                        {/* <h3 className="video-title">{post.frontmatter.videoTitle}</h3> */}
-                        <p dangerouslySetInnerHTML={{ __html: post.html }} />
-                    </div>
-                    <div className="col-sm-12 col-md-7 video-frame">
-                        <Video videoSrcURL={post.frontmatter.videoSourceUrl}>
-
-                        </Video>
-                    </div>
-                    {/* <h1>{post.frontmatter.videoTitle}</h1>
+      <Layout>
+        <SEO title={post.frontmatter.videoTitle} />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-5 featured-video">
+              <h1 className="site-title">{post.frontmatter.videoTitle}</h1>
+              <hr />
+              {/* <h2>Music Composition & Sound Design</h2> */}
+              {/* <h3 className="video-title">{post.frontmatter.videoTitle}</h3> */}
+              <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            </div>
+            <div className="col-sm-12 col-md-7 video-frame">
+              <Video videoSrcURL={post.frontmatter.videoSourceUrl}></Video>
+            </div>
+            {/* <h1>{post.frontmatter.videoTitle}</h1>
                         <img src={post.frontmatter.videoThumbnail} />
                         <p dangerouslySetInnerHTML={{ __html: post.html }} /> */}
-                </div>
-                <VideoItems />
-
-            </div>
-        </Layout>
-
-
+          </div>
+          <VideoItems />
+        </div>
+      </Layout>
     )
 }
 
